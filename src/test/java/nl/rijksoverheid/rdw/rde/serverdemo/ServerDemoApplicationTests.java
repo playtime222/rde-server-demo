fdddf867d1afcf526f24e72327531cdb79108892
playtime222 <steve.kellaway@mefitihe.com>
2022-03-28 16:29:21 +0200

    first

---
+++ src/test/java/nl/rijksoverheid/rdw/rde/serverdemo/ServerDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.rijksoverheid.rdw.rde.serverdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
